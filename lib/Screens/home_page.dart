239514d3ddfadec4dc13bf36ebb9fc6511eb572b
MeghaChirayathPaulson <meghapaulson2000@gmail.com>
2021-06-09 17:42:01 +0530

    new commit

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
 
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+     body: SafeArea(
+       child: SingleChildScrollView(
+         child: Column(
+           children: [
+             Container(
+               padding: EdgeInsets.symmetric(vertical: 20,horizontal: 20),
+               width: double.infinity,
+               color: Colors.purple,
+               child: Text('Portfolio'),
+             )
+           ],
+
+         ),
+         ),
+       ),
+    );
   }
 }
 
