751830ec61f7d5f39318fea9e3c927d8b5a9440b
MeghaChirayathPaulson <meghapaulson2000@gmail.com>
2021-06-09 18:41:32 +0530

    new commit

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 
@@ -8,13 +10,25 @@ class HomePage extends StatelessWidget {
      body: SafeArea(
        child: SingleChildScrollView(
          child: Column(
+           crossAxisAlignment: CrossAxisAlignment.center,
            children: [
              Container(
                padding: EdgeInsets.symmetric(vertical: 20,horizontal: 20),
                width: double.infinity,
                color: Colors.purple,
-               child: Text('Portfolio'),
-             )
+               child: Text('Portfolio',textAlign: TextAlign.center,style: TextStyle(fontWeight: FontWeight.w800),),
+             ),
+             const SizedBox(height: 30,),
+             CircleAvatar(
+               radius: 40,
+               backgroundImage: NetworkImage('https://images.unsplash.com/photo-1608068071962-63140de5de69?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=334&q=80'),
+             ),
+             Text('Megha Paulson',style: TextStyle(fontWeight: FontWeight.w600),),
+             Text('Developer',style: TextStyle(fontWeight: FontWeight.w400),),
+             const SizedBox(height: 30,),
+             Text('Hai',style: TextStyle(fontWeight: FontWeight.w600),),            
+             Text('I am Megha Chirayath Paulson studying Computer Science and Engineering at Christ College of Engineering.I am budding developer with a lot of enthusiasm towards technology',
+             textAlign: TextAlign.start,)
            ],
 
          ),
